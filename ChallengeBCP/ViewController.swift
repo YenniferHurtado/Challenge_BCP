09c86f4ebe401f38deb32c2b5a5c6ea710925d38
Yennifer Hurtado <yennifer.hurtadoarce@gmail.com>
2023-11-15 18:09:20 -0500

    Initial Commit

---
+++ ChallengeBCP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeBCP
+//
+//  Created by Yennifer Hurtado Arce on 15/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
